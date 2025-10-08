0d1501b43e11331a934366a24e5d3f4665ed7dcc
Theo Long <theodorelong98@gmail.com>
2025-10-08 14:56:42 -0400

    remove

--- src/ast.jl
+++ src/ast.jl
@@ -18,20 +18,3 @@ mutable struct Block <: Program
     commands::Vector{Command}
     retvar::Union{Symbol,Nothing}
 end
-
-function Base.show(io::IO, p::Block)
-    for command in p.commands
-        println("\t", command)
-    end
-    print("return", p.retvar)
-end
-
-function Base.show(io::IO, c::Assign)
-    println("Assign ", c.symbol, " := ", c.f)
-    print("\tFree ", c.free)
-end
-
-function Base.show(io::IO, c::Unassign)
-    println("Unassign ", c.symbol, " := ", c.f)
-    print("\tFree ", c.free)
-end

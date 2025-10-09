a0a57463b3328c8fcaf7119eb56f592b035106d9
Theo Long <theodorelong98@gmail.com>
2025-10-09 13:54:53 -0400

    add constraint function

--- src/utils.jl
+++ src/utils.jl
@@ -8,4 +8,31 @@ mapM(f, xs) = @prob begin
     end
 end
 
-export mapM
+"""
+Given some probabilistic program, constrain some random draws to have specific values.
+
+Currently only supports constraining the value of variables which appear in the return statement
+"""
+function constrain(model::Block, constraints::Dict{Symbol,<:Any})
+    retval_assignments = [c for c in model.commands if c.symbol == model.retvar]
+    symbols = keys(constraints)
+    @assert length(retval_assignments) == 1
+    retval_symbols = first(retval_assignments).free
+    if !all([s in retval_symbols for s in symbols])
+        throw(
+            ErrorException("Can only constrain variables that appear in the return statement: $([s for s in symbols if !(s in retval_symbols)])")
+        )
+    end
+    new_commands = []
+    for command in model.commands
+        if typeof(command) == Assign && command.symbol in symbols
+            new_command = Assign(command.symbol, () -> dirac(constraints[command.symbol]), command.free)
+        else
+            new_command = command
+        end
+        push!(new_commands, new_command)
+    end
+    return Block(new_commands, model.retvar)
+end
+
+export mapM, constrain
